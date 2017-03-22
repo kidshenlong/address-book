e0624fc1af330247359e2c0e1e99c9c9dbedec0d
kidshenlong <michaelpm91@googlemail.com>
2017-03-22 19:30:18 +0000

    Answered first question

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,26 +2,24 @@ import java.io.File
 
 import com.github.tototoshi.csv.CSVReader
 
-import scala.io.Source
-
 /**
   * Created by Michael on 22/03/2017.
   */
 object Main extends App{
 
-  val bufferedSource = Source.fromFile("src/main/resources/AddressBook.csv")
-
-  bufferedSource.getLines().foreach(println)
-
-  bufferedSource.close()
+  case class Person(name: String, gender: String, dateOfBirth: String)
 
   val reader = CSVReader.open(new File("src/main/resources/AddressBook.csv"))
 
-  reader.all().foreach{ line =>
-
-    println(s"Name: ${line(0)} | ${line(1)} | ${line(2)}")
-
+  val people = reader.all().map{ line =>
+    val trimmedLine = line.map(_.trim)
+    Person( name = trimmedLine.head, gender = trimmedLine(1), dateOfBirth = trimmedLine(2))
   }
-
   reader.close()
+
+  println("How many males are in the address book?")
+  println(people.collect{
+    case p@Person(_, "Male", _) => p
+  }.length)
+
 }
